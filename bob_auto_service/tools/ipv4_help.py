3235b6d623016159253e5e56616e1c1733ab1877
Maue <chris.maue@intelligrated.com>
2017-09-19 10:58:34 -0500

    Added some logging statements for debug purposes

--- bob_auto_service/tools/ipv4_help.py
+++ bob_auto_service/tools/ipv4_help.py
@@ -21,7 +21,8 @@ __status__ = "Development"
 # IPv4 Format helper function *************************************************
 def check_ipv4(address, logger=None):
     """ simple function used to determine if the contents of a string are
-    compatable with an ipv4 address """    # Configure loggers
+    compatable with an ipv4 address """
+    # Configure loggers
     logger = logger or logging.getLogger(__name__)
 
     ipv4_regex = r'\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.' \
@@ -32,10 +33,15 @@ def check_ipv4(address, logger=None):
     if isinstance(address, str) is not True:
         try:
             address = str(address)
+            logger.debug('Input value was successfully converted to string')
         except Exception:
             return False
+    else:
+        logger.debug('Input value was not a string: %s', str(address))
     # check if address is formatted correctly for an ipv4 address
     if re.fullmatch(ipv4_regex, address) is not None:
+        logger.debug('Provided address is a valid IP address: %s', address)
         return True
     else:
+        logger.debug('Provided address is NOT a valid IP address: %s', address)
         return False
--- bob_auto_service/tools/ref_num.py
+++ bob_auto_service/tools/ref_num.py
@@ -25,10 +25,11 @@ __status__ = "Development"
 # Class definition ************************************************************
 class RefNum(object):
     def __init__(self, logger=None):
-        # Configure logger
+        # Configure loggers
         self.logger = logger or logging.getLogger(__name__)
 
         # Init tags
+        self.logger.debug('Setting source register to initial value of 100')
         self._source = 100
 
     # source control **********************************************************
@@ -50,7 +51,10 @@ class RefNum(object):
 
     # new value control *******************************************************
     def new(self):
+        self.logger.debug('Incrementing source register')
         self._source += 1
         if self._source > 999:
+            self.logger.debug('Rolling over source register')
             self._source = 100
+        self.logger.debug('Returning source value to main: %d', self._source)
         return str(self._source)
